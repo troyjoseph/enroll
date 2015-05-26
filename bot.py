f77f174350bbcea9aa0b82b39d7325b7c3f2c5f2
Troy Joseph <tcj29@cornell.edu>
2015-05-26 14:02:17 -0400

    bot4

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-#OSX ONLY b/c of use pf command+t, change for windows
-
-from splinter import Browser
-import requests, thread
+import requests, thread, os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,26 +8,39 @@ netid ='tcj29'
 password='tc2013j@cony'
 url = requests.get('http://studentcenter.cornell.edu', allow_redirects = True).url
 
-driver = webdriver.Firefox()
+chromedriver = "/Users/troy/Downloads/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+
+def addClass(className, cookies):
+	print className
+	_driver = webdriver.Chrome(chromedriver)
+	_driver.add_cookies(cookies)
+	_driver.get(baseurl)
+
+
+driver = webdriver.Chrome(chromedriver)
+
 try:
 	driver.get(url)
 	driver.find_element_by_id('netid').send_keys(netid)
 	driver.find_element_by_id('password').send_keys(password)
 	driver.find_element_by_name('Submit').click()
+	driver.find_element_by_id('DERIVED_SSS_SCL_LINK_ADD_ENRL').click()
 
 	baseurl = driver.current_url
+	cookies = driver.get_cookies()
+
 	thread.start_new_thread( addClass, ("name", ) )
 
 except Exception,e: print str(e)
+
+while(True):
+	continue 
 
 #finally:
 #	driver.close()
 
 
-def addClass(className):
-	print className
-	_driver = webdriver.Firefox()
-	_driver.get(baseurl)
-	body = _driver.find_element_by_tag_name("body")
-	
+
 	
